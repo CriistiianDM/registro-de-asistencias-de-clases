f1fc7d871c86f47b9c3a5aaca98c90522f2498c4
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2021-12-22 22:47:34 -0500

    cambios nuevos

--- src/components/components-inicio/CajaBox.jsx
+++ src/components/components-inicio/CajaBox.jsx
@@ -1,8 +1,9 @@
 /**
  *  @decs import libs
 */
-import React  from "react";
-import  json  from "../../json/components_states.json";
+import React from "react";
+import $ from 'jquery';
+import json from "../../json/components_states.json";
 import style_cajaBox from "../../css/style_cajaBox.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
@@ -10,23 +11,29 @@ import { faUserTie } from "@fortawesome/free-solid-svg-icons";
 import { faUsersCog } from "@fortawesome/free-solid-svg-icons";
 
 export function CajaBox(props) {
-    
+
     let state_boxCaja = props.properties
-    let iconState = [faGraduationCap,faUserTie,faUsersCog] 
+    let iconState = [faGraduationCap, faUserTie, faUsersCog]
 
     return (
-        <div className={state_boxCaja["cls-1"]}> 
-           <div></div>
-           <div className={state_boxCaja["cls-2"]}>
-               <div className={state_boxCaja["cls-3"]}>
-                   <FontAwesomeIcon icon={ iconState[state_boxCaja["icon-caja"]] }  className={state_boxCaja["cls-4"]} />
-               </div>
-               <div className={state_boxCaja["cls-5"]}>
-                   <div className={state_boxCaja["cls-6"]}>{state_boxCaja.title}</div>
-                   <div></div>
-               </div>
-           </div>
-           <div></div>
+        <div className={state_boxCaja["cls-1"]}>
+            <div></div>
+            <div className={state_boxCaja["cls-2"]}>
+                <div className={state_boxCaja["cls-3"]}>
+                    <FontAwesomeIcon icon={iconState[state_boxCaja["icon-caja"]]} className={state_boxCaja["cls-4"]} />
+                </div>
+                <div className={state_boxCaja["cls-5"]}>
+                    <div className={state_boxCaja["cls-6"]}>{state_boxCaja.title}</div>
+                    <div className={state_boxCaja["cls-7"]}>
+                        <ul>
+                            {(state_boxCaja.description).map((value) => (
+                                <li key={value}>{value}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div></div>
         </div>
     );
 }
